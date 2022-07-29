3f52562ea68eb5788a48a1aac2f59893a7fd5724
Adam Johnson <vaeb@hey.com>
2022-07-30 00:43:51 +0100

    Improve timeline

--- frontend/src/components/panels/TimelineMid.tsx
+++ frontend/src/components/panels/TimelineMid.tsx
@@ -1,11 +1,11 @@
 import {
-    Box, ScrollArea, Stack, Text, TextInput, TextInputProps, Title, Tooltip, useMantineTheme, 
+    Box, Button, ScrollArea, Stack, Text, Textarea, TextInput, TextInputProps, Title, Tooltip, useMantineTheme, 
 } from '@mantine/core';
 import React, { FC, useEffect, useRef } from 'react';
 import { BsPersonCircle as IconPerson } from 'react-icons/bs';
 import shallow from 'zustand/shallow';
 
-import { useGetPostsWeightedQuery, useSendPostMutation } from '../../generated/graphql';
+import { useGetPostsWeightedQuery, useMeQuery, useSendPostMutation } from '../../generated/graphql';
 import { useAppStore, useTimelineStore } from '../../state';
 // import { useAppStore, useConvoStore } from '../../state';
 import { formatTime, getDateString } from '../../utils/formatTime';
@@ -17,13 +17,15 @@ const getPostTime = (postDateRaw: Date | number, nowDate = new Date()) => {
 
 const TimelineMid: FC = () => {
     const theme = useMantineTheme();
-    // const [{ data: meData, fetching: meFetching }] = useMeQuery();
+    const [{ data: meData, fetching: meFetching }] = useMeQuery();
 
     const { posts, refreshedPosts } = useTimelineStore(state => ({ posts: state.posts, refreshedPosts: state.refreshedPosts }), shallow);
     const [, doSendPost] = useSendPostMutation();
     const setScrollToTop = useTimelineStore(state => state.setScrollToTop);
     const setView = useAppStore(state => state.setView);
     const inputRef = useRef<HTMLInputElement>(null);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    // const divtextRef = useRef<HTMLDivElement>(null);
     const scrollRef = useRef<HTMLInputElement>(null);
 
     // console.log('stale', stale);
@@ -31,7 +33,7 @@ const TimelineMid: FC = () => {
     // const posts = !postsFetching ? postsData?.getPostsWeighted?.posts : [];
     // console.log('posts', posts);
 
-    // const me = meData?.me;
+    const me = meData?.me;
     // const isMe = user => user.id == me.id;
 
     useEffect(() => {
@@ -39,15 +41,23 @@ const TimelineMid: FC = () => {
         setScrollToTop(() => scrollRef.current?.scrollTo({ top: 0, behavior: 'smooth' }));
     }, [setScrollToTop]);
 
-    const onKeyDown = (props: TextInputProps & React.RefAttributes<HTMLInputElement>) => {
-        const { key } = props;
-        if (key === 'Enter') {
-            const msg = inputRef.current.value;
-            doSendPost({
-                text: msg,
-            });
-            inputRef.current.value = '';
-        }
+    // const onKeyDown = (props: TextInputProps & React.RefAttributes<HTMLInputElement>) => {
+    //     const { key } = props;
+    //     // if (key === 'Enter') {
+    //     //     const msg = textareaRef.current.value;
+    //     //     doSendPost({
+    //     //         text: msg,
+    //     //     });
+    //     //     textareaRef.current.value = '';
+    //     // }
+    // };
+
+    const sendPost = () => {
+        const msg = textareaRef.current.value;
+        doSendPost({
+            text: msg,
+        });
+        textareaRef.current.value = '';
     };
 
     const onUserClick = (post: typeof posts[0]) => {
@@ -63,32 +73,60 @@ const TimelineMid: FC = () => {
             <ScrollArea className='grow px-0 pb-0' viewportRef={scrollRef} offsetScrollbars>
                 <Stack className='' spacing={23}>
                     <Box className='flex flex-col justify-end'>
-                        <TextInput
-                            autoComplete='off'
-                            ref={inputRef}
-                            className=''
-                            styles={{
-                                input: {
-                                    '::placeholder': {
-                                        fontSize: '18px',
-                                        color: theme.colors._gray[6],
+                        <Box className='flex w-full'>
+                            {/* <Box>
+                            <IconPerson className='text-_gray-800 h-10 w-10 mt-[15px] cursor-pointer' onClick={() => setView('user', null, me.id)} />
+                        </Box> */}
+                            <Textarea
+                                ref={textareaRef}
+                                // contentEditable='true' data-testid='tweetTextarea_0' role='textbox' spellCheck='true' tabIndex={0}
+                                autoComplete='off'
+                                className='grow'
+                                styles={{
+                                    input: {
+                                        '::placeholder': {
+                                            fontSize: '18px',
+                                            color: theme.colors._gray[6],
+                                        // lineHeight: '68px',
+                                        },
+                                        height: '90px',
+                                        minHeight: '70px',
+                                        paddingTop: '20px !important',
+                                        // lineHeight: '48px',
+                                        color: theme.colors._gray[8],
                                     },
-                                    height: '70px',
-                                    minHeight: '70px',
-                                    lineHeight: '68px',
-                                    color: theme.colors._gray[8],
-                                },
-                            }}
-                            size='xl'
-                            radius='md'
-                            placeholder={'What\'s on your mind?'}
-                            icon={<IconPerson className='h-10 w-10' color={theme.colors._gray[8]} />}
-                            onKeyDown={onKeyDown}
-                        />
+                                    icon: {
+                                        alignItems: 'flex-start',
+                                        marginTop: 15,
+                                    },
+                                }}
+                                size='xl'
+                                radius='md'
+                                rightSection={
+                                    <Box className='w-30 self-end mb-[14px]'>
+                                        <Button className='float-left' styles={{ root: { width: 130 } }} size='md' variant='gradient' onClick={sendPost}>Send post</Button>
+                                    </Box>
+                                }
+                                rightSectionWidth={160}
+                                rightSectionProps={{
+                                    className: 'flex flex-col self-end',
+                                }}
+                                placeholder={'What\'s on your mind?'}
+                                icon={<IconPerson className='h-10 w-10' color={theme.colors._gray[8]} />}
+                                // onKeyDown={onKeyDown}
+                                autosize
+                            // minRows={2}
+                            />
+                        </Box>
+                        {/* <Box>
+                            <Button className='float-left' size='sm' variant='default'>Send post</Button>
+                        </Box> */}
                     </Box>
                     {posts.map(post => (
-                        <Box className='flex items-center w-full text-_gray-800 px-[10px]' key={post.id}>
-                            <IconPerson className='h-10 w-10 cursor-pointer' onClick={() => onUserClick(post)} />
+                        <Box className='flex w-full text-_gray-800 px-[10px]' key={post.id}>
+                            <Box>
+                                <IconPerson className='h-10 w-10 mt-[5px] cursor-pointer' onClick={() => onUserClick(post)} />
+                            </Box>
                             <Stack ml={9} spacing={0}>
                                 <div className='flex gap-[5px] items-center'>
                                     <Text className='font-bold cursor-pointer' onClick={() => onUserClick(post)}>{post.creator.name}</Text>
